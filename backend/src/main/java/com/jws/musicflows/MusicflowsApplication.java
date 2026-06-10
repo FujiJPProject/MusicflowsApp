3f5e84c50809d9e47f12a458ee8fb13a569f43bb
fujisann1024 <tehepero302@gmail.com>
2026-06-10 23:40:09 +0900

    Spring bootプロジェクトの作成

---
+++ backend/src/main/java/com/jws/musicflows/MusicflowsApplication.java
@@ -0,0 +1,13 @@
+package com.jws.musicflows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicflowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicflowsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jws/musicflows/MusicflowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jws.musicflows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicflowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
