1092f4d2135d9aaef078540b12e4be1d8ff92aab
fujisann1024 <tehepero302@gmail.com>
2026-09-25 01:06:46 +0900

    カンマ忘れ修正

--- floci/init/ready.d/06-init-lambda.sh
+++ floci/init/ready.d/06-init-lambda.sh
@@ -100,7 +100,7 @@ environment = {
         "spring_cloud_function_definition": "musicJobWorker",
         "AWS_REGION": "${AWS_REGION}",
         "AWS_ENDPOINT_URL": "${AWS_EDGE_INTERNAL_URL}",
-        "FILE_BUCKET": "${FILE_BUCKET}"
+        "FILE_BUCKET": "${FILE_BUCKET}",
         "WORKER_PROCESSOR_TYPE": "LAMBDA"
     }
 }
